9c0f17c3be558f1673e5ff458b2c9d3cfa7a365a
antonnprince <antonprince95@gmail.com>
2024-02-25 15:02:05 +0530

    added assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
+import { Loader } from "../components/Loader"
 
+
+//packages needed - three/fiber and three/drei
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className="w-full h-screen relative">
+    {/*objects nearer than 0.1 and farer than 1000 wont be rendered */}   
+    {/*3d scene rendered here */}
+    <Canvas className="w-full h-screen bg-transparent"   
+   camera={{near: 0.1,far: 1000}}
+    >
+        {/*Loader component shown before 3d model rendered */}
+        <Suspense fallback={<Loader/>}>
+            <directionalLight />
+            <ambientLight />
+            <pointLight />
+            <spotLight />
+            <hemisphereLight />
+        </Suspense>
+    </Canvas>
+    </section>
   )
 }
 
