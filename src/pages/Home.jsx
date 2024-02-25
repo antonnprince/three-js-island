d01c337e30546ccb265251cfd7a3af00c5e06a6d
antonnprince <antonprince95@gmail.com>
2024-02-25 15:43:11 +0530

    loaded island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
+/* eslint-disable no-unused-vars */
 import { Canvas } from "@react-three/fiber"
 import { Suspense } from "react"
 import { Loader } from "../components/Loader"
-
+import Island from '../models/Island'
 
 //packages needed - three/fiber and three/drei
 const Home = () => {
+
+    const adjustIslandForScreenSize =()=>{
+        let screenScale = null
+        let screenPosition = [0, -6.5, -43]
+
+        if(window.innerWidth < 768) {
+            screenScale = [0.9, 0.9, 0.9]
+        }
+
+        else {
+            screenScale = [1, 1, 1]
+           
+        }
+
+        return [screenScale, screenPosition]
+    }
+
+        const [islandScale, islandPosition] = adjustIslandForScreenSize();
   return (
     <section className="w-full h-screen relative">
     {/*objects nearer than 0.1 and farer than 1000 wont be rendered */}   
@@ -19,6 +38,11 @@ const Home = () => {
             <pointLight />
             <spotLight />
             <hemisphereLight />
+
+            <Island 
+                position = {islandPosition}
+                scale = {islandScale}
+            />
         </Suspense>
     </Canvas>
     </section>
